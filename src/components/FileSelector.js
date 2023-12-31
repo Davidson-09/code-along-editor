c09f8fc2c0f6c36bfe58bbca16fff2d389757a99
Davidson-09 <davidsonakra@gmail.com>
2023-12-31 07:36:29 +0100

    specified the file tree interface in a comment

--- src/components/FileSelector.js
+++ src/components/FileSelector.js
@@ -1,37 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { UncontrolledTreeEnvironment, Tree, StaticTreeDataProvider } from 'react-complex-tree';
 import 'react-complex-tree/lib/style-modern.css';
 
 export default function FileSelector({fileTree}) {
-
-    const items = {
-        root: {
-          index: 'root',
-          isFolder: true,
-          children: ['child1', 'child2'],
-          data: 'Root item',
-        },
-        child1: {
-          index: 'child1',
-          children: [],
-          data: 'Child item 1',
-        },
-        child2: {
-          index: 'child2',
-          isFolder: true,
-          children: ['child3'],
-          data: 'Child item 2',
-        },
-        child3: {
-            index: 'child3',
-            children: [],
-            data: 'Child item 3',
-          }
-    }
-
-    useEffect(()=>{
-        console.log(fileTree, 'the damn tree')
-    }, [])
 
     return (
     <div className='absolute bg-gray-500 border border-black border-r-gray-700 z-10 top-0 '>
--- src/utility/buildFileTree.js
+++ src/utility/buildFileTree.js
@@ -1,6 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 
+// the file tree interface
+// const items = {
+//     root: {
+//       index: 'root',
+//       isFolder: true,
+//       children: ['child1', 'child2'],
+//       data: 'Root item',
+//     },
+//     child1: {
+//       index: 'child1',
+//       children: [],
+//       data: 'Child item 1',
+//     },
+//     child2: {
+//       index: 'child2',
+//       isFolder: true,
+//       children: ['child3'],
+//       data: 'Child item 2',
+//     },
+//     child3: {
+//         index: 'child3',
+//         children: [],
+//         data: 'Child item 3',
+//       }
+// }
+
 function buildTree(folderPath) {
     let fileItems = {
         root:{
